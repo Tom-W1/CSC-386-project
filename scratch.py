4ace1e1e0c372279135b7f8b2005f9ecc647208d
coxb <coxb@berea.edu>
2020-02-07 19:29:42 -0500

    Added Negative word text file

--- scratch.py
+++ scratch.py
@@ -15,10 +15,10 @@ def get_submission():
     brain = []
     for i in bb_plays:
         brain.append(i.title)
-    print (brain)
-
-
+    print(brain)
     return brain
+
+
 def print_submission_comments():
     auth_com = []
     author = ""
@@ -26,16 +26,17 @@ def print_submission_comments():
     for subs in bb_plays:
         comments = subs.comments
         for auth in comments:
-            author  = auth.author
+            author = auth.author
             comment = auth.body
-            auth_com.append(author.__str__()+':'+ comment)
-    print (auth_com)
-def save_submissions():
-    red_all = reddit.subreddit('depression').top(limit = 1000)
-    dict =        { "title":[],
-                    "body":[],
-                "subreddit":[]}
+            auth_com.append(author.__str__()+':' + comment)
+    print(auth_com)
 
+
+def save_submissions():
+    red_all = reddit.subreddit('depression').top(limit=1000)
+    dict = {"title":[],
+            "body":[],
+            "subreddit":[]}
 
     for submission in red_all :
         dict["title"].append(submission.title)
@@ -46,6 +47,7 @@ def save_submissions():
     df.to_csv('Freedom.csv')
     return df
 
+
 def process_submissions():
     df = pd.read_csv('Freedom.csv')
     num_subs = len(df["subreddit"])
@@ -53,23 +55,25 @@ def process_submissions():
     ary = {}
     for i in range(num_subs):
         title = df['title'][i]
-        sub  = df["subreddit"][i]
+        sub = df["subreddit"][i]
         ary[sub] = ary.get(sub, 0)+title.count('!')
-        diction[sub] =  diction.get(sub,0)+1
+        diction[sub] = diction.get(sub,0)+1
     subs = list(diction.items())
-    subs.sort(key  = lambda x:x [1],reverse = True)
-    mos_sub = subs[ :10]
+    subs.sort(key=lambda x: x[1], reverse=True)
+    mos_sub = subs[:10]
     print(subs)
     print(mos_sub)
-    for (k,v) in mos_sub:
+    for (k, v) in mos_sub:
         print(str(ary.get(k)) + ':' + k)
 
 
 def main():
     save_submissions()
     process_submissions()
+
+
 if __name__ == '__main__':
-     main()
+    main()
 
 
 
