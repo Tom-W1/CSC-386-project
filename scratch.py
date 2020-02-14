de9b6ceff645aeef2b7c1a8724d226a2e49fb2db
westth <Westth@gmail.com>
2020-02-14 14:20:16 -0500

    made some different CSV files for the different part and added some naming

--- scratch.py
+++ scratch.py
@@ -8,6 +8,50 @@ reddit = praw.Reddit(client_id='h75002hxSddmJA',
                      username='Krost303',
                      password='Freedom303!!')
 
+def save_title_submission():
+    """saves the title of the first thousand post of the depression reddit sorted by top for manipulation
+
+    : return: df
+    """
+    red_depress = reddit.subreddit('depression').top(limit=1000)
+    depression_dict_title = {"title": []}
+
+    for submission in red_depress:
+        depression_dict_title['title'].append(submission.title)
+
+    df = pd.DataFrame(depression_dict_title)
+    df.to_csv('post_title_text.csv')
+    return df
+
+def save_auth_submission():
+    """saves the authors of the first thousand post of the depression reddit sorted by top for manipulation
+
+    : return: df
+    """
+    red_depress = reddit.subreddit('depression').top(limit=1000)
+    depression_dict_author = {"author": []}
+
+    for submission in red_depress:
+        depression_dict_author['author'].append(submission.author)
+
+    df = pd.DataFrame(depression_dict_author)
+    df.to_csv('post_author.csv')
+    return df
+
+def save_body_submission():
+    """saves the body of the first thousand post of the depression reddit sorted by top for manipulation
+
+    : return: df
+    """
+    red_depress = reddit.subreddit('depression').top(limit=1000)
+    depression_dict_body = {"body": []}
+
+    for submission in red_depress:
+        depression_dict_body['body'].append(submission.selftext)
+
+    df = pd.DataFrame(depression_dict_body)
+    df.to_csv('post_body_text.csv')
+    return df
 
 def save_submissions():
     """
@@ -18,15 +62,15 @@ def save_submissions():
     red_all = reddit.subreddit('depression').top(limit=1000)
     depression_dict_all_info = {"title": [],
             "body": [],
-            "subreddit": []}
+            "redditor":[]}
 
     for submission in red_all:
         depression_dict_all_info["title"].append(submission.title)
-        depression_dict_all_info['subreddit'].append(submission.subreddit)
+        depression_dict_all_info['redditor'].append(submission.author)
         depression_dict_all_info['body'].append(submission.selftext)
 
-    df = pd.DataFrame(dict)
-    df.to_csv('Freedom.csv')
+    df = pd.DataFrame(depression_dict_all_info)
+    df.to_csv('total_submission_info.csv')
     return df
 
 
@@ -57,8 +101,9 @@ def process_submissions():
 
 def main():
     save_submissions()
-    process_submissions()
-
+    save_body_submission()
+    save_auth_submission()
+    save_title_submission()
 
 if __name__ == '__main__':
      main()
