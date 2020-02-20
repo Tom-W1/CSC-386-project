875417bcaf0065dab06e3e032ef996e4a54afd52
westth <Westth@gmail.com>
2020-02-19 19:39:28 -0500

    made some different CSV files for the different part and added some naming

--- scrapper.py
+++ scrapper.py
@@ -8,6 +8,22 @@ reddit = praw.Reddit(client_id='h75002hxSddmJA',
                      username='Krost303',
                      password='Freedom303!!')
 
+def save_score_submission():
+    """saves the title of the first thousand post of the depression reddit sorted by top for manipulation
+
+    : return: df
+    """
+    red_depress = reddit.subreddit('depression').top(limit=1000)
+    depression_dict_title = {"score": []}
+
+    for submission in red_depress:
+        depression_dict_title['score'].append(submission.score)
+
+    df = pd.DataFrame(depression_dict_title)
+    df.to_csv('post_score_text.csv')
+    return df
+
+
 def save_title_submission():
     """saves the title of the first thousand post of the depression reddit sorted by top for manipulation
 
@@ -60,43 +76,23 @@ def save_submissions():
     :return: df
     """
     red_all = reddit.subreddit('depression').top(limit=1000)
-    depression_dict_all_info = {"title": [],
+    depression_dict_all_info = {
+            "title": [],
             "body": [],
-            "redditor":[]}
+            "redditor": [],
+            "score": []       }
 
     for submission in red_all:
         depression_dict_all_info["title"].append(submission.title)
         depression_dict_all_info['redditor'].append(submission.author)
         depression_dict_all_info['body'].append(submission.selftext)
+        depression_dict_all_info['score'].append(submission.score)
+
 
     df = pd.DataFrame(depression_dict_all_info)
     df.to_csv('total_submission_info.csv')
     return df
 
-
-def process_submissions():
-    """
-    Looks at the title of the post and checks if it has a '!' in it
-    if it has one then it adds to a counter. At the end it prints the number
-    of posts with '!' in the title
-    :return:
-    """
-    df = pd.read_csv('Freedom.csv')
-    num_subs = len(df["subreddit"])
-    diction = {}
-    ary = {}
-    for i in range(num_subs):
-        title = df['title'][i]
-        sub = df["subreddit"][i]
-        ary[sub] = ary.get(sub, 0)+title.count('!')
-        diction[sub] = diction.get(sub, 0)+1
-    subs = list(diction.items())
-    subs.sort(key=lambda x: x[1], reverse=True)
-    mos_sub = subs[:10]
-    print(subs)
-    print(mos_sub)
-    for (k, v) in mos_sub:
-        print(str(ary.get(k)) + ':' + k)
 
 
 def main():
@@ -104,6 +100,7 @@ def main():
     save_body_submission()
     save_auth_submission()
     save_title_submission()
+    save_score_submission()
 
 if __name__ == '__main__':
      main()
